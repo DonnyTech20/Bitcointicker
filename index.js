1f40eab8d34a8b14e419ecbe919d72d38e6e5e67
Donny Techiera <DonnyTech20@gmail.com>
2019-03-25 13:36:37 -0400

    post rt

--- index.js
+++ index.js
@@ -8,8 +8,13 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 
-app.get("/", function(req, res) {
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", (req, res) => {
+    console.log(req.body.crypto);
+
 });
 
 
